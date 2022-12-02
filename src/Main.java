2f8f34bdbae3fbeaf6ac18ad79d792a973d820c1
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2022-12-02 09:41:44 +0100

    primera actualizacion

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hellow world");
+
+        System.out.println("Inicio del proyecto");
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
